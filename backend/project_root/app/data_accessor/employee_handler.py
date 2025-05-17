bd2ef234302d6e179aa21e7ee1ddf5f07760d216
Atiqul Islam <ai8646@mun.ca>
2025-05-16 23:25:48 -0230

    updated employee handler

--- backend/project_root/app/data_accessor/employee_handler.py
+++ backend/project_root/app/data_accessor/employee_handler.py
@@ -1,17 +1,15 @@
 from pathlib import Path
 from typing import List, Optional
 
-from data_models.employee import Employee
-from data_accessor.csv_handler import CsvHandler
+from app.data_models.employee import Employee
+from app.data_accessor.csv_handler import CsvHandler
 
 class EmployeeHandler(CsvHandler[Employee]):
     """
     Handler for Employee data loaded from a CSV file.
     """
     def __init__(self, file_path: Path):
-        # Initialize with the file path and Employee model
         super().__init__(file_path, Employee)
-        # Cache to avoid reloading CSV on each lookup
         self._cache: Optional[List[Employee]] = None
 
     def load_all(self) -> List[Employee]:
@@ -23,3 +21,11 @@ class EmployeeHandler(CsvHandler[Employee]):
     def get_by_id(self, employee_id: int) -> Optional[Employee]:
         """Retrieve an employee by their ID."""
         return next((e for e in self.load_all() if e.employee_id == employee_id), None)
+    
+    def get_employee_company_name(self, first_name: str, last_name: str) -> Optional[str]:
+        """Retreive name of a company associated to an employee"""
+        return next((e.company_name for e in self.load_all() if e.first_name == first_name and e.last_name == last_name), None)
+    
+    def get_employee_email(self, first_name: str, last_name: str) -> Optional[str]:
+        """Retreive the email of an employee"""
+        return next((e.email for e in self.load_all() if e.first_name == first_name and e.last_name == last_name), None)
