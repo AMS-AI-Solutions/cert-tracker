2750cacfaa17bc3372d2b291545b21796b558387
shemonto <shemonto21@gmail.com>
2025-05-19 10:03:29 -0230

    updated the course handler class

---
+++ backend/project_root/app/data_accessor/pandas/course_handler.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import Optional, Dict, Any
+from app.data_models.course import Course
+from app.data_accessor.pandas.pandas_handler import PandasHandler
+from app.data_accessor.interfaces.course_handler_interface import CourseHandlerInterface
+
+class CourseHandler(PandasHandler[Course], CourseHandlerInterface):
+    """
+    Concrete implementation of CourseHandlerInterface using PandasHandler.
+    """
+    def __init__(self, file_path: Path):
+        super().__init__(
+            file_path,
+            Course,
+            index_col="course_id"
+        )
+
+    def get_by_id(self, course_id: str) -> Optional[Course]:
+        """
+         Retrieve a Course object based on its unique course ID
+        """
+        try:
+            row_series = self.df.loc[course_id]
+        except KeyError:
+            return None
+        row_dict: Dict[str, Any] = row_series.to_dict()
+        row_dict["course_id"] = course_id
+        return Course(**{str(k): v for k, v in row_dict.items()})
+
+    def get_by_course_name_from_certificate_name(self, certificate_name: str) -> Optional[str]:
+        """
+        Retrieve the course name that corresponds to the given certificate name.
+        """
+        subset = self.df[self.df["certificate_name"] == certificate_name]
+        if subset.empty:
+            return None
+        return subset.iloc[0]["course_name"]
